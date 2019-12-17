2dcab893673f1301a936d61d7e03c13b98ca781f
Eric Energy <ericnrgnash@gmail.com>
2019-12-16 23:12:48 -0600

    finished out all functionality

--- config/orm.js
+++ config/orm.js
@@ -13,7 +13,7 @@ var orm = {
   },
 
   getthings: function (value, tableInput) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var queryString = "SELECT ?? FROM ??";
       connection.query(queryString, [value, tableInput], function (err, result) {
         if (err) {
@@ -30,8 +30,25 @@ var orm = {
     })
   },
 
+  getthings2: function (id, value, value2, tableInput) {
+    return new Promise(function (resolve, reject) {
+      var queryString = "SELECT ??, ??, ?? FROM ??";
+      connection.query(queryString, [id, value, value2, tableInput], function (err, result) {
+        if (err) {
+          reject(err);
+        };
+        var employeeInfo = [];
+        parsedInfo = JSON.parse(JSON.stringify(result));
+        for (i = 0; i < parsedInfo.length; i++) {
+          employeeInfo.push("Id# " + parsedInfo[i].id + " " + parsedInfo[i].first_name + " " + parsedInfo[i].last_name);
+        };
+        resolve(employeeInfo);
+      });
+    })
+  },
+
   getRolen: function (value, tableInput) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var queryString = "SELECT ?? FROM ??";
       connection.query(queryString, [value, tableInput], function (err, result) {
         if (err) {
@@ -48,9 +65,9 @@ var orm = {
     })
   },
 
-  
+
   getEmployee: function (value) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var queryString = "SELECT * FROM ??";
       connection.query(queryString, [value], function (err, result) {
         if (err) {
@@ -60,7 +77,7 @@ var orm = {
         parsedInfo = JSON.parse(JSON.stringify(result));
         console.log(parsedInfo)
         for (i = 0; i < parsedInfo.length; i++) {
-          departmentInfo.push("Id# "+ parsedInfo[i].id +" "+ parsedInfo[i].first_name +" "+ parsedInfo[i].last_name);
+          departmentInfo.push("Id# " + parsedInfo[i].id + " " + parsedInfo[i].first_name + " " + parsedInfo[i].last_name);
           console.log(departmentInfo)
 
         };
@@ -68,13 +85,42 @@ var orm = {
       });
     })
   },
+  //left join so i have acess to all the data  left join with
+  //SELECT column_name(s) FROM table1 LEFT JOIN table2 ON table1.column_name = table2.column_name;
+  // getDepartmentID: function (value) {
+  //   return new Promise(function (resolve, reject) {
+  //     var queryString = "SELECT  department.id, department.department_name, employee_db.role.title FROM  employee_db.department LEFT JOIN  employee_db.role ON department.id = role.department_id";
+  //     connection.query(queryString, [value], function (err, result) {
+  //       if (err) {
+  //         reject(err);
+  //       };
+        
+  //       var departmentInfo = [];
+  //       depparsedInfo = JSON.parse(JSON.stringify(result));
+  //       for (i = 0; i < depparsedInfo.length; i++) {
+  //         departmentInfo.push(depparsedInfo[i].id + " " + depparsedInfo[i].department_name);
+       
+        
+  //         var titleInfo = [];
+  //         titleparsedInfo = JSON.parse(JSON.stringify(result));
+  //         for (i = 0; i < titleparsedInfo.length; i++) {
+  //           if (titleparsedInfo[i].title === null){}
+  //           else {
+  //             titleInfo.push(titleparsedInfo[i].title );
+  //           }};
+  //         };
+  //       resolve(departmentInfo, titleInfo);
+  //     });
+  //   })
+  // },
+
 
 
   addDepartment: function (department_update) {
     var queryString = "INSERT INTO department (department_name) VALUES (?)";
     connection.query(queryString, [department_update], function (err, result) {
       if (err) throw err;
-      console.log(result + " was added");
+      console.log(JSON.stringify(department_update) + " was added");
       return process.exit(0);
     });
   },
@@ -83,7 +129,7 @@ var orm = {
     var queryString = "INSERT INTO role (title, salary, department_id, manager_id) VALUES (?, ?, ?, ?)";
     connection.query(queryString, [title, salary, departmentNum, managerNum], function (err, result) {
       if (err) throw err;
-      console.log("A new Role "+ title +" was added");
+      console.log("A new Role called " + title + " has been added");
       return process.exit(0);
     });
   },
@@ -92,11 +138,11 @@ var orm = {
     var queryString = "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)";
     connection.query(queryString, [fname, lName, roleNum], function (err, result) {
       if (err) throw err;
-      console.log("Congrants! "+ fname +" was added");
+      console.log("Congrants! " + fname + " was added");
       return process.exit(0);
     });
   },
-  
+
 
   deleteEmployeeBYId: function (value) {
     var queryString = "DELETE FROM `employee` WHERE `id` = ?;";
@@ -106,6 +152,29 @@ var orm = {
       return process.exit(0);
     });
   },
+
+
+  // UPDATE `members` SET `contact_number` = '0759 253 542' WHERE `membership_number` = 1;
+  //       table name       colomN NAME               new value           from id             #
+
+
+  updateYourHeartsDesire: function (tblName, colName, newValue, idNum) {
+    var queryString = "UPDATE ?? SET ?? = ? WHERE `id` = ?;";
+    connection.query(queryString, [tblName, colName, newValue, idNum], function (err, result) {
+      if (err) throw err;
+      return process.exit(0);
+    });
+  },
+
+  updateYourHeartsDesire2: function (tblName, colfName, val1, colLName, val2, idNum) {
+    var queryString = "UPDATE ?? SET ?? = ?, ?? = ? WHERE `id` = ?;";
+    connection.query(queryString, [tblName, colfName, val1, colLName, val2, idNum], function (err, result) {
+      if (err) throw err;
+      return process.exit(0);
+    });
+  },
+
+
 
 };
 
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function init() {
     name: "choices",
     type: "list",
     message: "What would you like to do?",
-    choices: ["Add", "View", "Terminate", "Update", "Exit"],
+    choices: ["Add", "View", "Terminate", "Update", "Exit", "Beep"],
   }).then((answer) => {
     if (answer.choices === "Add") {
       add();
@@ -38,6 +38,8 @@ function init() {
       terminateEmployee();
     } else if (answer.choices === "Update") {
       update();
+    } else if (answer.choices === "Beep") {
+      fun();
     } else {
       console.log("Goodbye!")
       return process.exit(0);
@@ -66,28 +68,29 @@ function add() {
           name: "title",
           type: "input",
           message: "What is the type of Role are you wanting to add?",
-        },{
+        }, {
           name: "salary",
           type: "input",
           message: "What salary will thise role have?",
-        },{
+        }, {
           name: "question1",
           type: "list",
           message: "What department will we be adding this role to?",
           choices: result,
-        },{
+        }, {
           name: "question2",
           type: "list",
           message: "Is this a managers role?",
           choices: ["Yes", "No"],
         }]).then((answer) => {
           let q2Answer;
-         var IndexNum = result.indexOf(answer.question1) + 1;
-         if (answer.question2 === "Yes"){
-          q2Answer = IndexNum;
-        }else{
-          q2Answer = "NULL";}
-        orm.addRole(answer.title, answer.salary, IndexNum, q2Answer);
+          var IndexNum = result.indexOf(answer.question1) + 1;
+          if (answer.question2 === "Yes") {
+            q2Answer = IndexNum;
+          } else {
+            q2Answer = "NULL";
+          }
+          orm.addRole(answer.title, answer.salary, IndexNum, q2Answer);
         });
       })
     } else if (answer.choices === "Employee") {
@@ -96,18 +99,18 @@ function add() {
           name: "firstName",
           type: "input",
           message: "First Name?",
-        },{
+        }, {
           name: "lastName",
           type: "input",
           message: "Last Name?",
-        },{
+        }, {
           name: "question1",
           type: "list",
           message: "What department will we be adding this role to?",
           choices: result,
         }]).then((answer) => {
-         var IndexNum = result.indexOf(answer.question1) + 1;
-        orm.addEmployee(answer.firstName, answer.lastName, IndexNum);
+          var IndexNum = result.indexOf(answer.question1) + 1;
+          orm.addEmployee(answer.firstName, answer.lastName, IndexNum);
         });
       })
     } else {
@@ -137,8 +140,8 @@ function view() {
   });
 };
 
-function terminateEmployee(){
-    orm.getEmployee("employee").then((result) => {
+function terminateEmployee() {
+  orm.getEmployee("employee").then((result) => {
     inquirer.prompt([{
       name: "question1",
       type: "list",
@@ -156,16 +159,158 @@ function terminateEmployee(){
 
 
 
-
-
-
-
-
-
-
+// UPDATE `members` SET `contact_number` = '0759 253 542' WHERE `membership_number` = 1;
+//       table name       colomN NAME               new value           from id             #
 
 
 
 function update() {
-  console.log("update")
+  inquirer.prompt({
+    name: "choices",
+    type: "list",
+    message: "What would you like to Update?",
+    choices: ["A Department", "A Role", "An Employees info"],
+  }).then((answer) => {
+    if (answer.choices === "A Department") {
+      inquirer.prompt({
+        name: "confirm1",
+        type: "confirm",
+        message: "You can only update a departments name at this time. Still proceed?",
+      }).then((answer) => {
+        console.log(answer)
+        if (answer.confirm1 === true) {
+          orm.getthings("department_name", "employee_db.department").then((result) => {
+            inquirer.prompt([{
+              name: "question1",
+              type: "list",
+              message: "Choose a departments name to update.",
+              choices: result,
+            }, {
+              name: "newbName",
+              type: "input",
+              message: "Please enter a new name.",
+            }]).then((answer) => {
+              var IndexNum = result.indexOf(answer.question1) + 1;
+              orm.updateYourHeartsDesire("department", "department_name", answer.newbName, IndexNum);
+            })});
+        } else {
+          return process.exit(0);
+        }
+      })
+    } else if (answer.choices === "A Role") {
+      inquirer.prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to see?",
+        choices: ["Change Title Name", "Change Salary Amount", "Change Manager ID"],  // "Change the Department ID of this role",
+      }).then((answer) => {
+        if (answer.choices === "Change Title Name"){ 
+          orm.getRolen("title", "employee_db.role").then((result) => {
+            inquirer.prompt([{
+            name: "question1",
+            type: "list",
+            message: "Choose a job title that you want to update.",
+            choices: result,
+          }, {
+            name: "newbName",
+            type: "input",
+            message: "What do you want to update this title to?",
+          }]).then((answer) => {
+            var IndexNum = result.indexOf(answer.question1) + 1;
+            orm.updateYourHeartsDesire("role", "title", answer.newbName, IndexNum);
+          })});
+
+        } else if(answer.choices === "Change Salary Amount"){
+          orm.getRolen("title", "employee_db.role").then((result) => {
+            inquirer.prompt([{
+            name: "question1",
+            type: "list",
+            message: "Choose a Role to change the salary to.",
+            choices: result,
+          }, {
+            name: "newbName",
+            type: "input",
+            message: "What do you want to update this salary to?",
+          }]).then((answer) => {
+            var IndexNum = result.indexOf(answer.question1) + 1;
+            orm.updateYourHeartsDesire("role", "salary", answer.newbName, IndexNum);
+          })});
+
+        // } else if(answer.choices === "Change the Department ID of this role"){
+        //   inquirer.prompt({
+        //     name: "confirm1",
+        //     type: "confirm",
+        //     message: "Please only update this if you think a role was added to the wrong department. Still proceed?",
+        //   }).then((answer) => {
+        //     if (answer.confirm1 === true) {
+        //       orm.getDepartmentID().then((result) => {
+        //         console.log(result)
+        //         inquirer.prompt([{
+        //           name: "question1",
+        //           type: "list",
+        //           message: "Choose a departments name to update.",
+        //           choices: result,
+        //         },{
+        //           name: "question2",
+        //           type: "list",
+        //           message: "Choose a departments name to update.",
+        //           choices: result,
+        //         },]).then((answer) => {
+        //           var IndexNum = result.indexOf(answer.question1) + 1;
+        //           var stepOne = JSON.stringify(answer.question1)
+        //           var stepTwo = stepOne.split(" ");
+        //           var stepThree = stepTwo[1]
+        //           orm.updateYourHeartsDesire("role", "department_id", stepThree, IndexNum);
+        //         })});
+        //     } else {
+        //       return process.exit(0);
+        //     }})
+        } else{
+          console.log("Goodbye!")
+          return process.exit(0);
+        }
+      });
+    } else if (answer.choices === "An Employees info") {
+      inquirer.prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to Update?",
+        choices: ["Update an employee Name", " Update an Employees ID Last Name"],
+      }).then((answer) => {
+        if (answer.choices === "Update an employee Name") {
+              orm.getthings2("id", "first_name", "last_name", "employee_db.employee").then((result) => {
+                inquirer.prompt([{
+                  name: "question1",
+                  type: "list",
+                  message: "Choose an employees name to update.",
+                  choices: result,
+                }, {
+                  name: "newFName",
+                  type: "input",
+                  message: "Please enter a new first name.",
+                },{
+                  name: "newLName",
+                  type: "input",
+                  message: "Please enter a new last name.",
+                }]).then((answer) => {
+                  var stepOne = JSON.stringify(answer.question1)
+                  var stepTwo = stepOne.split(" ");
+                  var stepThree = stepTwo[1]
+                  orm.updateYourHeartsDesire2("employee", "first_name", answer.newFName, "last_name", answer.newLName, stepThree);
+                })});    } else {
+      console.log("Due to the sensitivity of this info it cannot be changed at this time. Goodbye!")
+      return process.exit(0);
+    }})
+    } else {
+      console.log("Goodbye!")
+      return process.exit(0);
+    }
+  });
+};
+
+
+function fun() {
+
+  console.log("Beep... Yah thats all this does. What got got a problem? BEEP BEEP! IS THIS WHAT YOU REALLY WANT FROM ME? WILL THIS MAKE YOU HAPPY? BEEEEP BEEEPP...... Look, sorry I yelled, its been a long day, ill do better I promise.")
+  return process.exit(0);
 };
